6b9b99d8ee5b42850bac8a5eed778c6f2f493529
Arjun <arjunketha@gmail.com>
2025-02-10 14:55:53 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
@@ -20,6 +21,8 @@ const io=new Server(server)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// every client can communicate with server
+app.use(cors());
 
 app.use('/ui', bullServerAdapter.getRouter());
 
